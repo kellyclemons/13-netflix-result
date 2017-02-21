155b0fa7022df127b4f04e13c90bd50af89d6d7a
Kelly Clemons <kellyclemons@kellys-air.attlocal.net>
2017-02-20 23:55:13 -0600

    created fill-in-result-js and edited all js files

---
+++ app/fill-in-result.js
@@ -0,0 +1,11 @@
+// create a module that exports a function and accepts two arguments: element and data object
+export default function fillInResult(el, data) {
+  el.querySelector('.movie-info__name').innerText = data.show_title;
+  el.querySelector('.movie-info__genre').innerText = data.category;
+  el.querySelector('.rating__number').innerText = data.rating;
+  el.querySelector('.movie-info__image').innerText = data.poster;
+  el.querySelector('.summary-text').innerText = data.summary;
+  el.querySelector('.actors-text').innerText = data.show_cast;
+
+  // return data;
+}
--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
 import 'whatwg-fetch';
+
+import fillInResult from './fill-in-Result'
+
+const form = document.querySelectory('.search-bar__prompt');
+const button = document.querySelector('.search-bar__button');
+const result = document.querySelector('.result')
