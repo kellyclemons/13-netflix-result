80624a7b898cd90f65a6b84a0bbecb9aa8e4e9d1
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-02-21 16:13:46 -0600

    finished all js and styling

--- app/fill-in-result.js
+++ app/fill-in-result.js
@@ -3,9 +3,7 @@ export default function fillInResult(el, data) {
   el.querySelector('.movie-info__name').innerText = data.show_title;
   el.querySelector('.movie-info__genre').innerText = data.category;
   el.querySelector('.rating__number').innerText = data.rating;
-  el.querySelector('.movie-info__image').innerText = data.poster;
+  el.querySelector('.movie-info__image').src = data.poster;
   el.querySelector('.summary-text').innerText = data.summary;
   el.querySelector('.actors-text').innerText = data.show_cast;
-
-  // return data;
 }
--- app/index.js
+++ app/index.js
@@ -1,7 +1,27 @@
 import 'whatwg-fetch';
+import fillInResult from './fill-in-result';
 
-import fillInResult from './fill-in-Result'
-
-const form = document.querySelectory('.search-bar__prompt');
+const form = document.querySelector('.search-bar__title');
 const button = document.querySelector('.search-bar__button');
-const result = document.querySelector('.result')
+const result = document.querySelector('.container');
+
+// NOTES ON ABOVE ACTIONS
+// document.querySelector returns the first item in the document that matches the specified group of selectors;
+// tries to find the tage name '.search-bar__prompt' (use the dot to find the class name .search-bar__prompt)
+
+fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
+  .then(res => res.json()
+  ).then((data) => {
+    fillInResult(result, data);
+  });
+
+button.addEventListener('click', () => {
+  fetch(`http://netflixroulette.net/api/api.php?title=${form.value}`)
+    .then(res => res.json()
+    ).then((data) => {
+      fillInResult(result, data);
+    });
+
+
+  // window.alert('You have typed in {$searchInput.value}');
+});
